3b159d31ada97727c6840755a5f7603ad329e0a5
Girts Strazdins <girts.strazdins@gmail.com>
2023-01-01 11:50:04 +0100

    AuthenticatedExampleBase now supports HTTP logging

--- src/test/java/com/binance/api/examples/AuthenticatedExampleBase.java
+++ src/test/java/com/binance/api/examples/AuthenticatedExampleBase.java
@@ -2,6 +2,7 @@ package com.binance.api.examples;
 
 import com.binance.api.client.BinanceApiClientFactory;
 import com.binance.api.client.BinanceApiRestClient;
+import com.binance.api.client.config.BinanceApiConfig;
 import com.binance.api.client.security.ApiCredentials;
 
 /**
@@ -12,11 +13,26 @@ public abstract class AuthenticatedExampleBase {
   protected final BinanceApiRestClient client;
 
   /**
+   * Create new example with authenticated endpoints.
+   *
+   * @param useHttpLogging When true, debug-print all HTTP requests and responses
+   */
+  public AuthenticatedExampleBase(boolean useHttpLogging) {
+    client = createClient(useHttpLogging);
+  }
+
+  /**
    * Create an example, get API key and secret from the environment variables.
+   * Disable debugging by default
    */
   public AuthenticatedExampleBase() {
+    client = createClient(false);
+  }
+
+  private BinanceApiRestClient createClient(boolean useHttpLogging) {
     ApiCredentials credentials = ApiCredentials.creatFromEnvironment();
     BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance(credentials);
-    client = factory.newRestClient();
+    BinanceApiConfig.useHttpLogging = useHttpLogging;
+    return factory.newRestClient();
   }
 }
