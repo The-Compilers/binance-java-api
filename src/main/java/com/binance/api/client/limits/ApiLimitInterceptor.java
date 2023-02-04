f6081c6045cecc84a06603b09b096e2eac06e6b6
Girts Strazdins <girts.strazdins@gmail.com>
2023-02-04 15:25:15 +0100

    Bugfix: refresh the timestamp when sleeping before a request

--- src/main/java/com/binance/api/client/limits/ApiLimitInterceptor.java
+++ src/main/java/com/binance/api/client/limits/ApiLimitInterceptor.java
@@ -1,5 +1,6 @@
 package com.binance.api.client.limits;
 
+import okhttp3.HttpUrl;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -25,10 +26,10 @@ public class ApiLimitInterceptor implements Interceptor {
     long timeNow = System.currentTimeMillis();
     accountant.forgetOldApiCalls(timeNow);
     ApiCallWeights weights = getApiCallWeights(chain.request());
-    sleepIfApiLimitsExceeded(weights);
+    Request newRequest = sleepIfApiLimitsExceeded(weights, chain.request());
     accountant.addUsedWeights(weights, timeNow);
-
-    Response response = chain.proceed(chain.request());
+    debugPrint(newRequest.method() + " " + newRequest.url());
+    Response response = chain.proceed(newRequest);
     debugPrintUsedWeightHeaders(response);
 
     return response;
@@ -38,7 +39,7 @@ public class ApiLimitInterceptor implements Interceptor {
     for (String headerName : response.headers().names()) {
       if (headerName.startsWith("x-mbx-used-weight") || headerName.startsWith("x-mbx-order-count")
           || headerName.startsWith("x-sapi-used")) {
-        logger.info("  -> " +  headerName + ": " + response.header(headerName));
+        debugPrint("  -> " + headerName + ": " + response.header(headerName));
       }
     }
   }
@@ -58,18 +59,50 @@ public class ApiLimitInterceptor implements Interceptor {
       try {
         value = Integer.parseInt(headerValue);
       } catch (NumberFormatException e) {
-        logger.info("Invalid API weight header value: " + headerValue);
+        debugPrint("Invalid API weight header value: " + headerValue);
       }
     }
     return value;
   }
 
-  private void sleepIfApiLimitsExceeded(ApiCallWeights weights) {
+  private Request sleepIfApiLimitsExceeded(ApiCallWeights weights, Request request) {
     long timestamp = System.currentTimeMillis();
     long minSleepTime = ApiLimitAccountant.getInstance().getMinSleepDuration(weights, timestamp);
+    Request newRequest = request;
     if (minSleepTime > 0) {
-      sleep(minSleepTime);
+      sleep(minSleepTime + 1000);
+      newRequest = refreshTimestampHeader(request);
     }
+    return newRequest;
+  }
+
+  /**
+   * When sleeping, the request timestamp can become deprecated, let's refresh it
+   * with the current timestamp
+   */
+  private Request refreshTimestampHeader(Request originalRequest) {
+    String timestamp = originalRequest.url().queryParameter("timestamp");
+    Request newRequest = originalRequest;
+    if (timestamp != null) {
+      long newTimestamp = System.currentTimeMillis();
+      debugPrint("Updating timestamp from " + timestamp + " to " + newTimestamp);
+      newRequest = replaceTimestamp(originalRequest, newTimestamp);
+    }
+
+    return newRequest;
+  }
+
+  @NotNull
+  private static Request replaceTimestamp(Request originalRequest, long timestamp) {
+    Request.Builder newRequestBuilder = originalRequest.newBuilder();
+    HttpUrl updatedUrl = originalRequest.url().newBuilder().removeAllQueryParameters("timestamp")
+        .addQueryParameter("timestamp", "" + timestamp).build();
+    newRequestBuilder.url(updatedUrl);
+    return newRequestBuilder.build();
+  }
+
+  private static void debugPrint(String message) {
+    logger.info(message);
   }
 
   /**
